470a890f82ef22829df7c8b99b4c0103a2ded320
mich1991 <wozniak.michal91@gmail.com>
2022-07-28 22:11:10 +0100

    finish code refactor

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,14 +7,14 @@ const daysOfTheWeekObjects = [
     {name: 'Saturday', prefix: 'sa'},
     {name: 'Sunday', prefix: 'su'},
 ];
-const domInputElementsSufix = ['-s-hours','-s-minutes','-e-hours','-e-minutes','-b-hours','-b-minutes','-total-hours'];
+const domInputElementsSufix = ['-s-hours', '-s-minutes', '-e-hours', '-e-minutes', '-b-hours', '-b-minutes', '-total-hours'];
 const daysOftheWeek = {};
 
 daysOfTheWeekObjects.forEach(day => {
     let domElements = [];
     domElements.push(document.querySelector(`#${day.name}`))
     domInputElementsSufix.forEach(sufix => {
-       domElements.push(document.querySelector(`#${day.prefix}${sufix}`))
+        domElements.push(document.querySelector(`#${day.prefix}${sufix}`))
     })
     daysOftheWeek[day.name] = domElements;
 })
@@ -31,27 +31,15 @@ class Day {
         this.workMinuteBreak = minuteBreakEl;
         this.totalWorkTime = totalEl;
 
-        this.dayContainer.addEventListener('input' , () => {
-            const errorArray = [];
-            const inputsArray = [this.workHourStart, this.workMinuteStart, this.workHourEnd, this.workMinuteEnd, this.workHourBreak, this.workMinuteBreak]
-            inputsArray.forEach(input => {
-                errorArray.push(this.validateInput(input))
-            })
-            if (errorArray.includes(false)){
-                this.clearShiftHours()
-            } else {
-                console.log('everything fine')
-                this.totalWorkTime.innerText = this.countDayShift().toFixed(2);
-            }
-        })
+        this.dayContainer.addEventListener('input', this.validateDay())
     }
 
-    countDayShift(){
+    countDayShift() {
         let shiftDuration = 0;
         let startMinutes = (+this.workHourStart.value * 60) + +this.workMinuteStart.value;
-        let endMinutes =  (+this.workHourEnd.value * 60) + +this.workMinuteEnd.value;
+        let endMinutes = (+this.workHourEnd.value * 60) + +this.workMinuteEnd.value;
         let breakMinutes = (+this.workHourBreak.value * 60) + +this.workMinuteBreak.value;
-        if (this.workHourStart.value !== '' && this.workHourEnd.value !== '' ){
+        if (this.workHourStart.value !== '' && this.workHourEnd.value !== '') {
             if (startMinutes > endMinutes) {
                 let dayMaxMinutes = 24 * 60;
                 shiftDuration = ((dayMaxMinutes - startMinutes + endMinutes - breakMinutes) / 60).toFixed(2);
@@ -65,10 +53,12 @@ class Day {
             return 0;
         }
     }
-    clearShiftHours(){
-        this.totalWorkTime.innerText ='';
+
+    clearShiftHours() {
+        this.totalWorkTime.innerText = '';
     }
-    clearInputs(){
+
+    clearInputs() {
         this.workHourStart.value = '';
         this.workMinuteStart.value = '00';
         this.workHourEnd.value = '';
@@ -78,12 +68,25 @@ class Day {
         this.totalWorkTime.innerText = ''
     }
 
-    validateInput (input) {
+    validateDay() {
+        const errorArray = [];
+        const inputsArray = [this.workHourStart, this.workMinuteStart, this.workHourEnd, this.workMinuteEnd, this.workHourBreak, this.workMinuteBreak]
+        inputsArray.forEach(input => {
+            errorArray.push(this.validateSingleInput(input))
+        })
+        if (errorArray.includes(false)) {
+            this.clearShiftHours()
+        } else {
+            this.totalWorkTime.innerText = this.countDayShift().toFixed(2);
+        }
+    }
+
+    validateSingleInput(input) {
         const maxValue = +input.dataset.valid
         // Stackoverflow solution
         // https://stackoverflow.com/questions/33299639/allow-only-2-numbers-on-input-type-number
         input.value = input.value.replace(/[^0-9.]/g, '').replace(/(\..*?)\..*/g, '$1');
-        if(input.value > maxValue){
+        if (input.value > maxValue) {
             input.classList.add('error');
             // this.clearShiftHours();
             return false;
@@ -94,10 +97,11 @@ class Day {
         }
     }
 }
+
 //Day Class END =====
 // HoursCalculator Class START =====
 
-class HoursCalculator{
+class HoursCalculator {
     hoursCalculatorForm = document.querySelector('#hours-calculator')
     weekTotalHoursDOMel = document.querySelector('#week-total-hours');
     employeeID = 0
@@ -105,14 +109,14 @@ class HoursCalculator{
     employeeDisplayDOMel = document.querySelector('#employee-display')
     employeeName = document.querySelector('#employee')
 
-    constructor(monday,tuesday,wednesday,thursday,friday,saturday,sunday) {
-        this.Monday=monday
-        this.Tuesday=tuesday
-        this.Wednesday=wednesday
-        this.Thursday=thursday
-        this.Friday=friday
-        this.Saturday=saturday
-        this.Sunday=sunday
+    constructor(monday, tuesday, wednesday, thursday, friday, saturday, sunday) {
+        this.Monday = monday
+        this.Tuesday = tuesday
+        this.Wednesday = wednesday
+        this.Thursday = thursday
+        this.Friday = friday
+        this.Saturday = saturday
+        this.Sunday = sunday
         this.daysArray = [this.Monday, this.Tuesday, this.Wednesday, this.Thursday, this.Friday, this.Saturday, this.Sunday];
 
         this.hoursCalculatorForm.addEventListener('submit', this.handleSubmit.bind(this))
@@ -120,21 +124,21 @@ class HoursCalculator{
     }
 
     countWholeWeek = () => {
-        return this.daysArray.map(day => day.countDayShift()).reduce((a,c) => a + c, 0).toFixed(2);
+        return this.daysArray.map(day => day.countDayShift()).reduce((a, c) => a + c, 0).toFixed(2);
     }
 
     handleSubmit(e) {
         e.preventDefault();
         let name = this.employeeName.value
         let totalHours = this.countWholeWeek()
-        this.employeesArray.push({ id : this.employeeID, name, totalHours})
+        this.employeesArray.push({id: this.employeeID, name, totalHours})
         this.employeeID++
         this.clearParentElement(this.employeeDisplayDOMel);
         this.employeesArray.forEach(employee => this.appendDivToDOM(this.employeeDisplayDOMel, employee));
     }
 
 
-    clearCalculator(){
+    clearCalculator() {
         let days = [this.Monday, this.Tuesday, this.Wednesday, this.Thursday, this.Friday, this.Saturday, this.Sunday];
         days.forEach(day => day.clearInputs());
         this.employeeName.value = '';
@@ -158,6 +162,7 @@ class HoursCalculator{
         this.employeesArray.forEach(employee => this.appendDivToDOM(this.employeeDisplayDOMel, employee));
     }
 }
+
 // HoursCalculator Class END ====
 
 const Monday = new Day(...daysOftheWeek['Monday']);
@@ -167,4 +172,4 @@ const Thursday = new Day(...daysOftheWeek['Thursday']);
 const Friday = new Day(...daysOftheWeek['Friday']);
 const Saturday = new Day(...daysOftheWeek['Saturday']);
 const Sunday = new Day(...daysOftheWeek['Sunday']);
-const hoursCalculator = new HoursCalculator(Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday)
+const hoursCalculator = new HoursCalculator(Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday)
